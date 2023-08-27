6deaabd0d941a2056081e6ba624b42d48b71f85d
Thg <truxin2005@mail.ru>
2023-08-27 22:46:13 +0300

    New commit added at 2023-08-27 22:46:13

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use chrono::prelude::*;
 use std::process::Command;
 use std::fs::File;
 use std::io::prelude::*;
+use tokio::time::{Duration, interval};
 
 fn make_git_request(){
     let commit_message = format!("New commit added at {}",Local::now().format("%Y-%m-%d %H:%M:%S").to_string());
@@ -31,9 +32,19 @@ fn make_new_git_file()->std::io::Result<()>{
     Ok(())
 }
 
-fn main() {
-    if let Err(e) = make_new_git_file(){
-        eprintln!("An error occured: {}", e);
-    }
+#[tokio::main]
+async fn main() {
+    let first_interval = Duration::from_secs(30);
+    let mut ticker_files = interval(first_interval);
+    let second_interval = Duration::from_secs(5);
+    let mut ticker_git = interval(second_interval);
+
+    loop {
+        ticker_files.tick().await;
+        if let Err(e) = make_new_git_file(){
+            eprintln!("An error occured: {}", e);
+        }
     make_git_request();
+    }
+
 }
